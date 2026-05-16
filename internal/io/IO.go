b50fa4c2b7bc9a8cc5173b4ae28febeadc406f3d
XyL1GaN4eG <trolloguy@mail.ru>
2026-05-16 16:01:10 +0300

    add PrintError

--- internal/io/IO.go
+++ internal/io/IO.go
@@ -12,7 +12,12 @@ func SetOutput(w io.Writer) {
 	writer = w
 }
 
-func Output(msg string) error {
+func Print(msg string) error {
 	_, err := fmt.Fprintln(writer, msg)
+	return err
+}
+
+func PrintError(err error) error {
+	_, err = fmt.Fprintln(os.Stderr, err)
 	return err
 }
